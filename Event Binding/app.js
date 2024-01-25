364798b6fc06430d6890058d00e0466838bf45ad
muruganandam-nu <muruganandam.vg@nulogic.io>
2024-01-25 22:46:29 +0530

    Added two files

---
+++ Event Binding/app.js
@@ -0,0 +1,49 @@
+
+
+const app=Vue.createApp({
+    data:function(){
+        return {
+            count:0,
+            month:'',
+            fullMonth:""
+        }
+    },
+
+    watch:{
+        count(val){
+            if(val>50){
+                this.count=0
+            }
+        }
+
+    },
+
+    computed:{
+        fullMonth(){
+            console.log("called")
+            return this.fullMonth=this.month
+
+        }
+    },
+    methods:{
+        add(num){
+            this.count=this.count+num;
+        },
+        reduce(num){
+            this.count=this.count-num;
+        },
+        setMonth(event){
+            this.month=event.target.value
+        },
+        displayMonth(){
+            console.log("called")
+            if(this.month=='') return
+            
+            return this.fullMonth=`${this.month} 2024`
+
+        }
+
+    }
+})
+
+app.mount("#main")
